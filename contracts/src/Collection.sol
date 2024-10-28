b673952621561b982cb9b88042fe6aa19e95f6e8
Lea El Abboud <lea.s.abboud@gmail.com>
2024-10-28 18:10:53 +0100

    First onchain part

--- contracts/src/Collection.sol
+++ contracts/src/Collection.sol
@@ -1,12 +1,59 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8;
 
-contract Collection {
-  string public name;
-  int public cardCount;
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-  constructor(string memory _name, int _cardCount) {
-    name = _name;
+contract Collection is ERC721URIStorage, Ownable {
+  string public collectionName;
+  uint public cardCount;
+  uint private tokenIdCounter;
+  mapping(uint => Card) public cards; // mapping of token id to card
+
+
+  constructor(string memory _name, uint _cardCount) ERC721(_name, "PNFT") Ownable(msg.sender)  {
+    collectionName = _name;
     cardCount = _cardCount;
+    tokenIdCounter = 0;
   }
+
+  struct Card{
+    uint tokenId;
+    string tokenURI;
+  }
+  // Mint a new card with the next available 
+  //token ID (unique identifier for every nft card owned by the user) 
+  //and token URI (metadata of the card like img and card no)
+  
+  function mintCard(address _to, string memory _tokenURI) external onlyOwner returns (uint) {
+    require(tokenIdCounter < cardCount, "Token ID exceeds card count");
+    uint newTokenId = tokenIdCounter;
+    _mint(_to, tokenIdCounter);
+    _setTokenURI(tokenIdCounter, _tokenURI);
+    cards[newTokenId] = Card(newTokenId, _tokenURI);
+    tokenIdCounter++;
+    return newTokenId;
+  }
+
+  // get the no of cards minted
+  function getMintedCardCount() external view returns (uint) {
+      return tokenIdCounter;
+  }
+
+  // get the name of the collection
+  function getCollectionName() external view returns (string memory) {
+    return collectionName;
+  }
+  // get the no of cards of the collection
+  function getCollectionCardCount() external view returns (uint) {
+    return cardCount;
+  }
+
+  // 
+  function getCard(uint _tokenId) external view returns (Card memory) {
+    require(cards[_tokenId].tokenId == _tokenId, "Card does not exist");
+    return cards[_tokenId];
+  }
+
 }
--- contracts/src/Main.sol
+++ contracts/src/Main.sol
@@ -1,17 +1,94 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8;
 
+
 import "./Collection.sol";
 
-contract Main {
-  int private count;
-  mapping(int => Collection) private collections;
+contract Main is Ownable{
 
-  constructor() {
-    count = 0;
+  // ---------------------- Variables ----------------------
+  uint private collectionCounter;
+  mapping(uint => Collection) private collections;
+  mapping(address => uint[]) private userTokens; // mapping of user addresses to their tokenIds.
+  mapping(address => bool) private userExists; // to check if user exists
+  mapping(uint => uint) tokenToCollection; // mapping of token id to collection id (so we can get card's metadata based on the collection id)
+
+  constructor() Ownable(msg.sender) {
+    collectionCounter = 0;
   }
 
-  function createCollection(string calldata name, int cardCount) external {
-    collections[count++] = new Collection(name, cardCount);
+  address[] public users; // list of all users
+
+  // ---------------------- Events ----------------------
+  event CollectionCreated(address indexed collectionAddress, string collectionName, uint cardCount, uint indexed collectionId);
+  event CardMinted(address indexed to, uint tokenId, string tokenURI, uint indexed collectionId);
+
+  // ---------------------- Creation ----------------------
+
+  // Create a new collection with the given name and card count
+  function createCollection(string calldata _name, uint _cardCount) external onlyOwner {
+    require(_cardCount > 0, "Card count must be greater than 0");
+    Collection newCollection = new Collection(_name, _cardCount); 
+    collections[collectionCounter] = newCollection;
+    emit CollectionCreated(address(newCollection), _name, _cardCount, collectionCounter);
+    collectionCounter++;
+  }
+
+  // ---------------------- Functions ----------------------
+
+  // Mint a new card in the collection with the given collection ID
+  function mintNFTCard(address _to, string calldata _tokenURI, uint _collectionId) external onlyOwner {
+    require(_collectionId < collectionCounter, "Collection ID exceeds collection count");
+
+    Collection collection = collections[_collectionId];
+    uint newtokenId = collection.mintCard(_to, _tokenURI);
+
+    tokenToCollection[newtokenId] = _collectionId;
+
+    // add user to users list if not already added
+    if (!userExists[_to]) {
+      users.push(_to);
+      userExists[_to] = true; 
+    }
+    // add tokenId to userTokens mapping
+    userTokens[_to].push(newtokenId);
+
+    emit CardMinted(_to, newtokenId, _tokenURI, _collectionId);
+  }
+
+  // get list of tokenids of a user
+  function getUserTokens(address _user) internal view returns (uint[] memory) {
+    return userTokens[_user];
+  }
+
+  // get all users
+  function getUsers() external view returns (address[] memory) {
+    return users;
+  }
+
+  // get card info (needed for displaying card in frontend)
+  function getCard(uint _tokenId) internal view returns(Collection.Card memory) {
+    uint collectionId = tokenToCollection[_tokenId];
+    Collection collection = collections[collectionId];
+    return collection.getCard(_tokenId);
+   
+  }
+  // get cards of a user
+  function getUserCards(address _user) external view returns (Collection.Card[] memory) {
+    uint[] memory tokenIds = getUserTokens(_user);
+    Collection.Card[] memory userCards = new Collection.Card[](tokenIds.length);
+    for(uint i=0; i< tokenIds.length; i++){
+      userCards[i] = getCard(tokenIds[i]);
+    }
+    return userCards;
+  }
+
+  // get collection names (needed for displaying collection in frontend)
+  function getCollectionNames() external view returns (string[] memory) {
+    string[] memory collectionNames = new string[](collectionCounter);
+    for (uint i = 0; i < collectionCounter; i++) {
+      collectionNames[i] = collections[i].getCollectionName();
+    }
+    return collectionNames;
   }
 }
